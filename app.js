0b7a0dac44e3c919324bea83ff0cf7875e9bc5c9
Muhammad Nehal <78788867+cop3043@users.noreply.github.com>
2024-03-26 23:45:09 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,51 @@
+// Q#01
+
+// let city_name = prompt ("Please enter city name").toLocaleLowerCase();
+
+// if (city_name=="karachi") {
+// console.log ("Welcome to the city of lights")
+// }
+// else {
+// console.log("Welcome to " + city_name)
+// }
+
+
+// Q#2
+// let input_gender = prompt ("Please enter your Gender").toLowerCase();
+// if (input_gender=="male"){
+// console.log ("Good Morning Sir...!")
+// }
+// else if (input_gender=="female"){
+// console.log ("Good Morning Ma'am....!")
+// }else{
+//     console.log("Please enter valid input")
+// }
+
+// Q#3
+// let road_traffic_signal = prompt ("Please enter traffic signal").toLowerCase();
+// if (road_traffic_signal=="red"){
+// console.log ("Must Stop")
+// }
+// else if (road_traffic_signal=="yellow") {
+//  console.log ("Ready to Move")   
+// }
+// else if (road_traffic_signal=="green") {
+//     console.log ("Move now")
+// }
+// else {
+//     console.log("Please enter valid input")
+// }
+
+// Q#04
+
+// let fule_guage = prompt ("Please enter remaining fuel in the car").toLocaleLowerCase();
+
+// if (fule_guage<= 0.25) {
+// console.log ("Please refill the fuel in your car")
+// }
+// else {
+// console.log("Your car has ample fuel")
+// }
+
+// Q#06
+ 
